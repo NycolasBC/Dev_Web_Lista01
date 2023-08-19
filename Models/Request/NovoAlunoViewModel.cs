fd1532926ca44ea0d4fb3d076a984a23b7c858bd
NycolasBC <nycolascarvalho14@gmail.com>
2023-08-19 11:47:22 -0300

    Atv prática

---
+++ Models/Request/NovoAlunoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplicationList01.Models.Request
+{
+    public class NovoAlunoViewModel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
